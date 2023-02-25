497357d32313303c1bd5cb776c798a8f734a2c87
san <sankalp.saxena.sta@gmail.com>
2023-02-25 13:29:36 +0530

    Add for loop examples in bash scripting

---
+++ bash-scripting/loops-for-example2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for file in $(ls )
+do
+  echo "Processing file: $file"
+done
+
---
+++ bash-scripting/loops-for-example3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+values="apple orange banana"
+
+for fruit in $values
+do
+  echo "I like to eat $fruit"
+done
+
